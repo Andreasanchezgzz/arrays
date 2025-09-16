452191e2c1ec028b299e47cda4e60be904690dfd
Andreasanchezgzz <23308051220760@cbtis122.edu.mx>
2025-09-16 14:03:53 -0600

    Update exercise5.js

--- Leccion 02 Array/Ejercicio 05/exercise5.js
+++ Leccion 02 Array/Ejercicio 05/exercise5.js
@@ -2,3 +2,5 @@
 // Instrucciones: Cuenta cuántas espadas tienen un poder mayor a 50.
 const espadas = [20, 60, 45, 80, 90, 30];
 // Escribe tu solución aquí usando reduce():
+const Numespadas = espadas.reduce(espadas => espadas > 50);
+console.log (Numespadas);
