347665dda8ed915b9927ff35967d0b454db38148
Andreasanchezgzz <23308051220760@cbtis122.edu.mx>
2025-09-16 13:45:30 -0600

    Update exercise1.js

--- Leccion 02 Array/Ejercicio 01/exercise1.js
+++ Leccion 02 Array/Ejercicio 01/exercise1.js
@@ -2,3 +2,4 @@
 // Instrucciones: Filtra los objetos mágicos cuyo nivel de poder sea mayor a 50.
 const objetosMagicos = [20, 60, 45, 80, 90, 30];
 // Escribe tu solución aquí usando filter():
+const filtro = objetosMagicos.filter (objeto => objeto > 50);
