84af95613d9a0acd0f9c65d37f78d6fae4f68d5f
Andreasanchezgzz <23308051220760@cbtis122.edu.mx>
2025-09-16 13:52:00 -0600

    Update exercise2.js

--- Leccion 02 Array/Ejercicio 02/exercise2.js
+++ Leccion 02 Array/Ejercicio 02/exercise2.js
@@ -2,3 +2,5 @@
 // Instrucciones: Ordena los objetos mágicos de mayor a menor poder.
 const objetosMagicos = [20, 60, 45, 80, 90, 30];
 // Escribe tu solución aquí usando sort():
+const ordenar = objetosMagicos.sort()
+console.log (objetosMagicos);
